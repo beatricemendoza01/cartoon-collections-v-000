8757e1b32aacd865d5c1905d41b17f0753b8e91b
Beatrice Mendoza <mendoza@nyu.edu>
2018-09-05 04:38:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 def find_the_cheese(strings_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  strings_array.find 
+  strings_array.find { |string|  }
 end
